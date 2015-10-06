ad4469faa691a83ca967046f0efb01fdf17307c6
Arnaud TANGUY <arn.tanguy@gmail.com>
2015-10-05 22:14:09 -0700

    Cleanup connection sequence

--- src/wifi.cpp
+++ src/wifi.cpp
@@ -1,10 +1,10 @@
 
 /**
  * @example ConnectWiFi.ino
- * @brief The ConnectWiFi demo of library WeeESP8266. 
- * @author Wu Pengfei<pengfei.wu@itead.cc> 
+ * @brief The ConnectWiFi demo of library WeeESP8266.
+ * @author Wu Pengfei<pengfei.wu@itead.cc>
  * @date 2015.03
- * 
+ *
  * @par Copyright:
  * Copyright (c) 2015 ITEAD Intelligent Systems Co., Ltd. \n\n
  * This program is free software; you can redistribute it and/or
@@ -25,70 +25,75 @@
 
 #define SSID  "elecom2g-fa7d7b"
 #define PASSWORD  "4208401361893"
-#define HOST_NAME   "192.168.250.225"
+#define HOST_NAME "192.168.250.225"
 #define HOST_PORT   (5005)
 
-SoftwareSerial Serial1(2,3); // TX, RX
+SoftwareSerial Serial1(2, 3); // TX, RX
 ESP8266 wifi(Serial1);
- 
+
 void setup(void)
 {
-    Serial.begin(115200);  
-    Serial.print("setup begin\r\n");
-    
-    Serial.print("FW Version:");
-    Serial.println(wifi.getVersion().c_str());
-      
-    if (wifi.setOprToStationSoftAP()) {
-        Serial.print("to station + softap ok\r\n");
-    } else {
-        Serial.print("to station + softap err\r\n");
-    }
- 
-    if (wifi.joinAP(SSID, PASSWORD)) {
-        Serial.print("Join AP success\r\n");
-        Serial.print("IP: ");
-        Serial.println(wifi.getLocalIP().c_str());       
-    } else {
-        Serial.print("Join AP failure\r\n");
-    }
-    
-    // if (wifi.disableMUX()) {
-    //     Serial.print("single ok\r\n");
-    // } else {
-    //     Serial.print("single err\r\n");
-    // }
-    
-    Serial.print("setup end\r\n");
+  Serial.begin(115200);
+  Serial.print("setup begin\r\n");
+
+  Serial.print("Restarting WIFI: ");
+  if(wifi.restart()) {
+    Serial.print("OK\r\n");
+  } else {
+    Serial.print("FAILED");
+  }
+  Serial.print("FW Version: ");
+  Serial.println(wifi.getVersion().c_str());
+
+  Serial.print("To Station + SoftAP: ");
+  if (wifi.setOprToStationSoftAP()) {
+    Serial.print("OK\r\n");
+  } else {
+    Serial.print("FAILED\r\n");
+  }
+
+  if (wifi.joinAP(SSID, PASSWORD)) {
+    Serial.print("Join AP: OK\r\n");
+    Serial.print("IP: ");
+    Serial.println(wifi.getLocalIP().c_str());
+  } else {
+    Serial.print("Join AP: FAILED\r\n");
+  }
+
+  Serial.print("Single Mode: ");
+  if (wifi.disableMUX()) {
+    Serial.print("OK\r\n");
+  } else {
+    Serial.print("FAILED\r\n");
+  }
+
+  while (wifi.registerUDP(HOST_NAME, HOST_PORT)) {
+    Serial.print("register UDP: FAILED, retrying...\r\n");
+    wifi.unregisterUDP();
+    wifi.registerUDP(HOST_NAME, HOST_PORT);
+  }
+  Serial.print("register UDP: OK\r\n");
+
+
+  Serial.print("setup end\r\n");
 }
- 
+
 void loop(void)
 {
-    uint8_t buffer[128] = {0};
-    
-    if (wifi.registerUDP(HOST_NAME, HOST_PORT)) {
-        Serial.print("register udp ok\r\n");
-    } else {
-        Serial.print("register udp err\r\n");
+  uint8_t buffer[128] = {0};
+
+  const char *hello = "Hello, this is client!\n";
+  wifi.send((const uint8_t *)hello, strlen(hello));
+
+  uint32_t len = wifi.recv(buffer, sizeof(buffer), 1000);
+  if (len > 0) {
+    Serial.print("Received:[");
+    for (uint32_t i = 0; i < 64; i++) {
+      Serial.print((char)buffer[i]);
     }
-    
-    char *hello = "Hello, this is client!\n";
-    wifi.send((const uint8_t*)hello, strlen(hello));
-    
-    uint32_t len = wifi.recv(buffer, sizeof(buffer), 10000);
-    if (len > 0) {
-        Serial.print("Received:[");
-        for(uint32_t i = 0; i < len; i++) {
-            Serial.print((char)buffer[i]);
-        }
-        Serial.print("]\r\n");
-    }
-    
-    if (wifi.unregisterUDP()) {
-        Serial.print("unregister udp ok\r\n");
-    } else {
-        Serial.print("unregister udp err\r\n");
-    }
-    delay(1000);
+    Serial.print("]\r\n");
+  }
+
+  delay(1000);
 }
-          
+
--- test_server.py
+++ test_server.py
@@ -4,13 +4,17 @@ import socket
 
 # Listen on any address
 UDP_IP = ""
+UDP_SEND_IP = "192.168.250.228"
 # On port 
 UDP_PORT = 5005
+client_address = (UDP_SEND_IP, UDP_PORT)
 
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 sock.bind((UDP_IP, UDP_PORT))
 
 while True:
+    print "Sending message to %s" % (client_address, )
+    sent = sock.sendto("Hello from Server", client_address)
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    print "received message:", data
+    print "received message: %s from %s" % (data, addr, )
