c80048c3abab825afcc5d89733cfd291ae2fe114
Arnaud TANGUY <arn.tanguy@gmail.com>
2016-01-16 18:30:11 +0100

    Communicate via TCP :D

--- src/wifi.cpp
+++ src/wifi.cpp
@@ -23,12 +23,14 @@
 #include <Arduino.h>
 #include <SoftwareSerial.h>
 
-#define SSID  "elecom2g-fa7d7b"
-#define PASSWORD  "4208401361893"
-#define HOST_NAME "192.168.250.225"
-#define HOST_PORT   (5005)
+//#define SSID  "elecom2g-fa7d7b"
+//#define PASSWORD  "4208401361893"
+#define SSID "Sharkopter"
+#define PASSWORD "Sharkopter"
+#define HOST_NAME "192.168.4.100"
+#define HOST_PORT (12000)
 
-SoftwareSerial Serial1(2, 3); // TX, RX
+SoftwareSerial Serial1(2, 3);  // TX, RX
 ESP8266 wifi(Serial1);
 
 void setup(void)
@@ -37,43 +39,94 @@ void setup(void)
   Serial.print("setup begin\r\n");
 
   Serial.print("Restarting WIFI: ");
-  if(wifi.restart()) {
+  if (wifi.restart())
+  {
     Serial.print("OK\r\n");
-  } else {
+  }
+  else
+  {
     Serial.print("FAILED");
   }
   Serial.print("FW Version: ");
   Serial.println(wifi.getVersion().c_str());
 
-  Serial.print("To Station + SoftAP: ");
-  if (wifi.setOprToStationSoftAP()) {
-    Serial.print("OK\r\n");
-  } else {
-    Serial.print("FAILED\r\n");
+  Serial.print("Creating AP Sharkopter,Sharkopter");
+  // Set to AP mode
+  if (wifi.setOprToSoftAP())
+  {
+    Serial.println("Soft AP mode: OK");
+  }
+  else
+  {
+    Serial.println("Soft AP mode: ERROR");
   }
 
-  if (wifi.joinAP(SSID, PASSWORD)) {
-    Serial.print("Join AP: OK\r\n");
-    Serial.print("IP: ");
-    Serial.println(wifi.getLocalIP().c_str());
-  } else {
-    Serial.print("Join AP: FAILED\r\n");
+  // Setup the AP
+  if (wifi.setSoftAPParam(SSID, PASSWORD))
+  {
+    Serial.println("Soft AP setup successful");
+  }
+  else
+  {
+    Serial.println("Soft AP setup not successful");
+  }
+  Serial.println(wifi.getLocalIP().c_str());
+
+  // Serial.print("To Station + SoftAP: ");
+  // if (wifi.setOprToStationSoftAP()) {
+  //   Serial.print("OK\r\n");
+  // } else {
+  //   Serial.print("FAILED\r\n");
+  // }
+
+  // if (wifi.joinAP(SSID, PASSWORD)) {
+  //   Serial.print("Join AP: OK\r\n");
+  //   Serial.print("IP: ");
+  //   Serial.println(wifi.getLocalIP().c_str());
+  // } else {
+  //   Serial.print("Join AP: FAILED\r\n");
+  // }
+
+  // Serial.print("Single Mode: ");
+  // if (wifi.disableMUX()) {
+  //   Serial.print("OK\r\n");
+  // } else {
+  //   Serial.print("FAILED\r\n");
+  // }
+
+  // while (wifi.registerUDP(HOST_NAME, HOST_PORT)) {
+  //   Serial.print("register UDP: FAILED, retrying...\r\n");
+  //   wifi.unregisterUDP();
+  //   wifi.registerUDP(HOST_NAME, HOST_PORT);
+  // }
+  // Serial.print("register UDP: OK\r\n");
+
+  if (wifi.enableMUX())
+  {
+    Serial.print("multiple ok\r\n");
+  }
+  else
+  {
+    Serial.print("multiple err\r\n");
   }
 
-  Serial.print("Single Mode: ");
-  if (wifi.disableMUX()) {
-    Serial.print("OK\r\n");
-  } else {
-    Serial.print("FAILED\r\n");
+  if (wifi.startTCPServer(8090))
+  {
+    Serial.print("start tcp server ok\r\n");
+  }
+  else
+  {
+    Serial.print("start tcp server err\r\n");
   }
 
-  while (wifi.registerUDP(HOST_NAME, HOST_PORT)) {
-    Serial.print("register UDP: FAILED, retrying...\r\n");
-    wifi.unregisterUDP();
-    wifi.registerUDP(HOST_NAME, HOST_PORT);
+  if (wifi.setTCPServerTimeout(10))
+  {
+    Serial.print("set tcp server timout 10 seconds\r\n");
   }
-  Serial.print("register UDP: OK\r\n");
-
+  else
+  {
+    Serial.print("set tcp server timout err\r\n");
+  }
 
   Serial.print("setup end\r\n");
 }
@@ -81,19 +134,76 @@ void setup(void)
 void loop(void)
 {
   uint8_t buffer[128] = {0};
+  uint8_t mux_id;
+  uint32_t len = wifi.recv(&mux_id, buffer, sizeof(buffer), 100);
+  if (len > 0)
+  {
+    Serial.print("Status:[");
+    Serial.print(wifi.getIPStatus().c_str());
+    Serial.println("]");
 
-  const char *hello = "Hello, this is client!\n";
-  wifi.send((const uint8_t *)hello, strlen(hello));
-
-  uint32_t len = wifi.recv(buffer, sizeof(buffer), 1000);
-  if (len > 0) {
-    Serial.print("Received:[");
-    for (uint32_t i = 0; i < 64; i++) {
+    Serial.print("Received from :");
+    Serial.print(mux_id);
+    Serial.print("[");
+    for (uint32_t i = 0; i < len; i++)
+    {
       Serial.print((char)buffer[i]);
     }
     Serial.print("]\r\n");
+
+    if (wifi.send(mux_id, buffer, len))
+    {
+      Serial.print("send back ok\r\n");
+    }
+    else
+    {
+      Serial.print("send back err\r\n");
+    }
+
+    // if (wifi.releaseTCP(mux_id))
+    // {
+    //   Serial.print("release tcp ");
+    //   Serial.print(mux_id);
+    //   Serial.println(" ok");
+    // }
+    // else
+    // {
+    //   Serial.print("release tcp");
+    //   Serial.print(mux_id);
+    //   Serial.println(" err");
+    // }
+
+    Serial.print("Status:[");
+    Serial.print(wifi.getIPStatus().c_str());
+    Serial.println("]");
   }
 
-  delay(1000);
-}
+  // Serial.print("Joined device IPs: \r\n");
+  // Serial.print(wifi.getJoinedDeviceIP().c_str());
+  // Serial.print("IP Status: \r\n");
+  // Serial.print(wifi.getIPStatus().c_str());
 
+  // uint8_t buffer[128] = {0};
+
+  // const char *hello = "Hello, this is client!\n";
+  // Serial.print("Sending: Hello this is client!\r\n");
+  // if (wifi.send((const uint8_t *)hello, sizeof(uint8_t) * strlen(hello)))
+  // {
+  //   Serial.print("Message sent\r\n");
+  // }
+  // else
+  // {
+  //   Serial.print("FAILED\r\n");
+  // }
+
+  // // uint32_t len = wifi.recv(buffer, sizeof(buffer), 1000);
+  // // if (len > 0) {
+  // //   Serial.print("Received:[");
+  // //   for (uint32_t i = 0; i < 64; i++) {
+  // //     Serial.print((char)buffer[i]);
+  // //   }
+  // //   Serial.print("]\r\n");
+  // // }
+
+  // delay(1000);
+}
--- test_server.py
+++ test_server.py
@@ -1,20 +1,18 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 import socket
 
-# Listen on any address
-UDP_IP = ""
-UDP_SEND_IP = "192.168.250.228"
-# On port 
-UDP_PORT = 5005
-client_address = (UDP_SEND_IP, UDP_PORT)
 
-sock = socket.socket(socket.AF_INET, # Internet
-                     socket.SOCK_DGRAM) # UDP
-sock.bind((UDP_IP, UDP_PORT))
+TCP_IP = '192.168.4.1'
+TCP_PORT = 8090
+BUFFER_SIZE = 1024
+MESSAGE = "Hello, World!"
 
-while True:
-    print "Sending message to %s" % (client_address, )
-    sent = sock.sendto("Hello from Server", client_address)
-    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    print "received message: %s from %s" % (data, addr, )
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((TCP_IP, TCP_PORT))
+s.send(MESSAGE)
+data = s.recv(BUFFER_SIZE)
+s.close()
+
+print "received data:", data
+
